383bcaaf0f2f14ca4dbfe71d1c4fec09391d252a
hassantimothy <hassantimothy422@gmail.com>
2022-12-08 09:11:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,15 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 #include <unistd.h>
 
-
-
 /**
  *
- *  * main - prints exactly and that piece of art is useful
+ * main - prints exactly and that piece of art is useful
  *
- *   *
  *
  *    * Return: Always 0 (Success)
  *
- *     */
+     */
 
 int main(void)
 
