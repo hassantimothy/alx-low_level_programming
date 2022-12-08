3b5711c43fd5912406f834011f1759ef04b84266
hassantimothy <hassantimothy422@gmail.com>
2022-12-08 10:50:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+
+
 /**
  *
- *   main - Entry point on this task
+ *   main - Entry point
+ *
+ *
  *
  *   Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 
 }
