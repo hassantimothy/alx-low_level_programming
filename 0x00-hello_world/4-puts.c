4cd2570babcd447d0873a4598fc9dedba094a74d
hassantimothy <hassantimothy422@gmail.com>
2022-12-08 10:40:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,15 @@
 
 /**
  *
- *   main - Entry point
+ *   main - Entry point on this task
  *
  *   Return: Always 0 (Success)
- *
  */
 
 int main(void)
 
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
