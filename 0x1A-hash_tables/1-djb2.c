6481b346297f0f8e679dcb6541dbf122b6f2e9ca
hassantimothy <hassantimothy422@.com>
2023-04-09 11:44:14 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,11 +8,11 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int b;
+	int c;
 
 	hash = 5381;
-	while ((b = *str++))
-		hash = ((hash << 5) + hash) + b; /* hash * 33 + b */
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
 	return (hash);
 }
